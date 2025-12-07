b6166cd111428a71f3b18c146ae8ae19da0e2cf7
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-12-07 10:59:12 +0100

    Solve other one

---
+++ easy/1523/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int result = (high - low)/2;
+        
+        if (low % 2 == 1 && high % 2 == 1)
+            result++;
+        else{
+            if (low % 2 == 1)
+                result++;
+            if (high % 2 == 1)
+                result++;
+        }
+        return result;
+    }
+};
