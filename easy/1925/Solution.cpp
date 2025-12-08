33d98849bf0a24449659b67ccdaf037acc36e844
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-12-08 23:58:13 +0100

    modifer

--- easy/1925/Solution.cpp
+++ easy/1925/Solution.cpp
@@ -5,15 +5,14 @@ public:
     int countTriples(int n) {
         int count = 0;
         
-        // Iterate through all possible values for a and b
+
         for (int a = 1; a <= n; ++a) {
             for (int b = 1; b <= n; ++b) {
                 int sumSquares = a * a + b * b;
                 
-                // Calculate the integer square root
+
                 int c = sqrt(sumSquares);
-                
-                // Check if c is a perfect square AND if c is within the limit n
+
                 if (c <= n && c * c == sumSquares) {
                     count++;
                 }
