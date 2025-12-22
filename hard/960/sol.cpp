e94a66dc4ece66edd8bcc497d42e582b2c5f3c83
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-12-22 22:39:44 +0100

    Solve hard problem

---
+++ hard/960/sol.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        vector<int> dp(m, 1);
+        int max_len = 1;
+
+        for (int i = 1; i < m; ++i) {
+            for (int j = 0; j < i; ++j) {
+                bool possible = true;
+                for (int k = 0; k < n; ++k) {
+                    if (strs[k][j] > strs[k][i]) {
+                        possible = false;
+                        break;
+                    }
+                }
+                if (possible) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            max_len = max(max_len, dp[i]);
+        }
+
+        return m - max_len;
+    }
+};
